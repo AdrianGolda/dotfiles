48ed447a320573cd610af1a41e0126e54f7c477b
milochadam <miloch.adam@gmail.com>
2019-03-16 18:45:38 +0100

    dotfiles update

--- make_links.sh
+++ make_links.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-ln -s $PWD/.bash_aliases ~
-ln -s $PWD/.zshrc ~
-ln -s $PWD/.vimrc ~
-ln -s $PWD/.tmux.conf ~
+ln -s $PWD/.bash_aliases $HOME
+ln -s $PWD/.zshrc $HOME
+ln -s $PWD/.tmux.conf $HOME
+
+ln -s $PWD/.vimrc $HOME
+ln -s $PWD/.vim/.config $HOME/.vim
+ln -s $PWD/.vim/UltiSnips $HOME/.vim
